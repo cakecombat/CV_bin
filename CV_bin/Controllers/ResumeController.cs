cca22ecb73a40e4a210d9eab5ffecc4260216456
Ifeoluwa Janet Ibukun-ojo <iibukunojo@gmail.com>
2024-09-06 02:09:17 +0100

    Add project files.

---
+++ CV_bin/Controllers/ResumeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CV_bin.Models;
+
+namespace CV_bin.Controllers
+{
+    public class ResumeController : Controller
+    {
+        private readonly ResumeModels _resumeModel;
+
+        public ResumeController()
+        {
+            _resumeModel = new ResumeModels();
+        }
+
+        public IActionResult Resume(int page = 1)
+        {
+            int totalPages = 6;
+            if (page < 1) page = 1;
+            if (page > totalPages) page = totalPages;
+
+            ViewBag.Page = page;
+            return View(_resumeModel);
+        }
+
+        [HttpPost]
+        public IActionResult CalculateTax(decimal income)
+        {
+            decimal tax = 0;
+            if (income <= 300000)
+            {
+                tax = income * 0.07m;
+            }
+            else if (income <= 600000)
+            {
+                tax = 300000 * 0.07m + (income - 300000) * 0.11m;
+            }
+            else if (income <= 1100000)
+            {
+                tax = 300000 * 0.07m + 300000 * 0.11m + (income - 600000) * 0.15m;
+            }
+            else
+            {
+                tax = 300000 * 0.07m + 300000 * 0.11m + 500000 * 0.15m + (income - 1100000) * 0.19m;
+            }
+
+            ViewBag.Tax = tax;
+            ViewBag.Income = income;
+            ViewBag.Page = 6; // Show tax results on the tax page
+            return View("Resume", _resumeModel);
+        }
+    }
+}
